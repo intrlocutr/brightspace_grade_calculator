05b2e15004a9c4b563f8bd52e29b15d93cfae905
intrlocutr <45053302+intrlocutr@users.noreply.github.com>
2021-11-23 00:21:50 -0500

    fix for tables w/o headings

--- grader.js
+++ grader.js
@@ -5,9 +5,17 @@ var numCols = rows[0].cells.length;
 
 // the index of the column to tally
 // ignore heading rows, e.g., "Homework", "Labs"
-// so it will be off by 1 because of blank cell on far left of children
+// it will be off by 1 because of blank cell on far left of non-heading rows
 // tally the weight column if it is there, otherwise the points column
 var pointsCol = rows[0].cells[2].textContent == "Weight Achieved" ? 3 : 2;
+
+// if the last row is NOT offset,
+// this means there are NO heading rows
+var noHeadingRows = false;
+if (rows[rows.length - 1].cells.length == numCols) {
+    noHeadingRows = true;
+    pointsCol -= 1;
+}
 
 // points earned
 var totalPoints = 0;
@@ -16,15 +24,15 @@ var maxPoints = 0;
 for (var i = 1; i < rows.length; i++) {
     // since heading rows have the same # of columns as the first row,
     // look only for rows with more columns
-    // again, the blank space on the left is a column
-    if (rows[i].cells.length > numCols) {
+    // or, if this table, has no heading rows, do all rows
+    if (noHeadingRows || rows[i].cells.length > numCols) {
 	// score is formatted "earned / possible"
 	var points = rows[i].cells[pointsCol].textContent.split(" / ");
 	// if earned points is "-", don't count it
 	if (points[0] != "-") {
-	    totalPoints += Number(points[0]);
-	    // if nothing to the right of the slash, that score is extra credit
-	    maxPoints += points[1] ? Number(points[1]) : 0;
+            totalPoints += Number(points[0]);
+            // if nothing to the right of the slash, that score is extra credit
+            maxPoints += points[1] ? Number(points[1]) : 0;
 	}
     }
 }
